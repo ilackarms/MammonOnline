74509e5eab103c1d68f089a340b7a776c6c30988
Scott Weiss <sdw35@cornell.edu>
2017-01-15 12:36:58 -0500

    template for login gui

--- client/js/main.js
+++ client/js/main.js
@@ -1,18 +1,78 @@
-// function main() {
+window.onload = main;
+
+function main(){
     var socket = io();
-    socket.on('welcome', function (data) {
-        console.log('welcome to the server: ', data);
+    socket.on('connected', function (data) {
+        console.log('welcome to the server bro: ', data);
         socket.emit('asdf', 'hihi');
     });
 
+    var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', {
+        preload: function () {},
+        create: function () {
+            loadGUI();
+        },
+        update: function () {},
+        render: function () {}
+    });
+}
 
+function loadGUI() {
+    var guiObj = {
+        id: 'myWindow',
 
-// var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', {
-    //     preload: function () {},
-    //     create: function () {},
-    //     update: function () {},
-    //     render: function () {}
-    // });
-// }
-//
-// main();
+        component: 'Window',
+
+        padding: 4,
+
+        //component position relative to parent
+        position: {x: 10, y: 10},
+
+        width: 500,
+        height: 500,
+
+        layout: [1, 3],
+        children: [
+            null,
+            {
+                id: 'myInput',
+                text: '',
+                component: 'Input',
+                position: 'center',
+                width: 300,
+                height: 50,
+                font: {
+                    size: '25px',
+                    family: 'Exocet',
+                    color: '#9aff97'
+                }
+            },
+            {
+                id: 'btn1',
+                text: 'Get Text Value',
+                component: 'Button',
+                position: 'center',
+                width: 200,
+                height: 100,
+                font: {
+                    size: '25px',
+                    family: 'Exocet',
+                    color: '#6ba1ff'
+                }
+            }
+        ]
+    };
+
+    //load EZGUI themes
+    //here you can pass multiple themes
+    EZGUI.Theme.load(['assets/gui/themes/metalworks-theme/metalworks-theme.json'], function () {
+
+        //create the gui
+        //the second parameter is the theme name, see metalworks-theme.json, the name is defined under __config__ field
+        var guiElt = EZGUI.create(guiObj, 'metalworks');
+
+        EZGUI.components.btn1.on('click', function (event) {
+            alert(EZGUI.components.myInput.text);
+        });
+    });
+}
