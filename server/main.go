66f7cbb39de627aa463b4f4c8c848906d3e10418
ilackarms <sdw35@cornell.edu>
2017-01-19 12:40:51 -0500

    main function calls handlers.RegisterHanlders()

--- server/main.go
+++ server/main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
-	"log"
+	log "github.com/Sirupsen/logrus"
 	"net/http"
 
 	"github.com/googollee/go-socket.io"
+	"github.com/ilackarms/MammonOnline/server/enums"
+	"github.com/ilackarms/MammonOnline/server/handlers"
+	"github.com/ilackarms/MammonOnline/server/stateful"
 )
 
 func main() {
+	state := &stateful.State{
+		PersistentState: &stateful.PersistentState{},
+		EphemeralState:  &stateful.EphemeralState{},
+	}
+
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-		so.Emit("welcome", "newuser")
-		so.On("asdf", func(msg string) {
-			log.Println("recieved:", msg)
-			//so.BroadcastTo("chat", "chat message", msg)
-		})
+	server.On(enums.SERVER_EVENTS.CONNECTION.String(), func(so socketio.Socket) {
+		log.Infof("New Socket %v Connected, adding handlers...", so.Id())
+		handlers.RegisterHandlers(state, so)
 		so.On("disconnection", func() {
-			log.Println("on disconnect")
+			log.Println("TODO: disconnection HANDLER")
 		})
 	})
 	server.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
+		log.Println("TODO: error HANDLER:", err)
 	})
 
 	http.Handle("/socket.io/", server)
